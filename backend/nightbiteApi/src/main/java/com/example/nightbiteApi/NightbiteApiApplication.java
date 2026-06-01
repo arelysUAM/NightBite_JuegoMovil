3c477d774431a1d11095f9c134be203f96403174
Arelys Castillo <abrigitteo@uamv.edu.ni>
2026-06-01 17:53:34 -0600

    build: agregar proyecto base de API Spring Boot

---
+++ backend/nightbiteApi/src/main/java/com/example/nightbiteApi/NightbiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nightbiteApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightbiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightbiteApiApplication.class, args);
+	}
+
+}
---
+++ backend/nightbiteApi/src/test/java/com/example/nightbiteApi/NightbiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nightbiteApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightbiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
