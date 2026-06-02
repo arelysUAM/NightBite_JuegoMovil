d9831b93c6d4e11574883d86d77f7ef2184b2139
Arelys Castillo <abrigitteo@uamv.edu.ni>
2026-06-02 15:16:07 -0600

    build: agregar iconos extendidos de Material

--- mobile/nightbiteApp/app/build.gradle.kts
+++ mobile/nightbiteApp/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.compose.material.icons.extended)
 
     // Navigation Compose
     implementation(libs.navigation.compose)
