282a856feb13dae36f0da89f9f2bd413b9bf33a6
Arelys Castillo <abrigitteo@uamv.edu.ni>
2026-06-02 14:48:37 -0600

    feat: configurar orientación horizontal de la app

--- mobile/nightbiteApp/app/src/main/java/ni/edu/uam/nightbiteapp/ui/screens/LoginScreen.kt
+++ mobile/nightbiteApp/app/src/main/java/ni/edu/uam/nightbiteapp/ui/screens/LoginScreen.kt
@@ -1,25 +1,50 @@
 package ni.edu.uam.nightbiteapp.ui.screens
 
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 /**
  * Pantalla inicial de inicio de sesión.
  *
- * En esta etapa funciona como interfaz base para navegar al registro
- * o al menú principal de la aplicación.
+ * Permite navegar al registro o al menú principal de la aplicación.
  */
 @Composable
 fun LoginScreen(
     onNavigateToRegister: () -> Unit,
-    onNavigateToHome: () -> Unit
+    onNavigateToHome: () -> Unit,
+    onExitApp: () -> Unit
 ) {
+    val context = LocalContext.current
+    var lastBackPressTime by remember { mutableLongStateOf(0L) }
+
+    BackHandler {
+        val currentTime = System.currentTimeMillis()
+
+        if (currentTime - lastBackPressTime < 2000) {
+            onExitApp()
+        } else {
+            lastBackPressTime = currentTime
+            Toast.makeText(
+                context,
+                "Presiona nuevamente para salir",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
